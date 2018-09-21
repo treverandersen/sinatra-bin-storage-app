0c28d85b0fd562f7b19bbddec9b3e994f0f056f3
treverandersen <trever.r.andersen@gmail.com>
2018-09-21 10:54:02 -0500

    setup a post controller action to create a user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,18 @@ class UsersController < ApplicationController
 		erb :'/users/create_user'
 	end
 
-	
+	post '/signup' do 
+		if params[:username] == "" || params[:password] == ""
+			redirect "/signup"
+		else
+			@user = User.new(:username => params[:username], :password => params[:password])
+			if @user.save
+				session[:user_id] = @user.id 
+				redirect "/"
+			else
+				erb :'/users/create_user'
+			end
+		end
+	end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_21_145834) do
+ActiveRecord::Schema.define(version: 2018_09_21_155144) do
 
   create_table "bins", force: :cascade do |t|
     t.text "name"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 2018_09_21_145834) do
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "password_digest"
+    t.string "password"
   end
 
 end
