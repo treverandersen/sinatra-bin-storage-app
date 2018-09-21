8f2803e2badb5cbfbf61378b9feb523435fed3d6
treverandersen <trever.r.andersen@gmail.com>
2018-09-21 11:07:05 -0500

    added helpers methods to check if a user is logged in, setup log in and log out controller actions, added condition to homepage to check if user is is logged in and if so let them log out

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,4 +12,14 @@ class ApplicationController < Sinatra::Base
     erb :index
 	end
 	
+	helpers do 
+		def logged_in?
+			!!current_user
+		end
+
+		def current_user
+			@current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+		end
+	end
+	
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,4 +18,30 @@ class UsersController < ApplicationController
 		end
 	end
 
+	get '/login' do 
+		if !logged_in?
+			erb :'/users/login'
+		else
+			redirect "/"
+		end
+	end
+
+	post '/login' do 
+		user = User.find_by(:username => params[:username])
+		if user && user.authenticate(params[:password])
+			session[:user_id] = user.id 
+			redirect "/"
+		else
+			redirect "/signup"
+		end
+	end
+
+	get '/logout' do 
+		if logged_in? 
+			session.destroy
+			redirect "/login"
+		else
+			redirect "/"
+		end
+	end
 end
