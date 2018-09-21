e4a205a845fc830ad9f413688cdee557f31f93e7
treverandersen <trever.r.andersen@gmail.com>
2018-09-21 11:12:59 -0500

    setup a user slug so it makes sense for how we get to a users show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,10 @@
 class UsersController < ApplicationController
 
+	get '/users/:slug' do 
+		@user = User.find_by_slug(params[:slug])
+		erb :'/users/show'
+	end
+
 	get '/signup' do
 		erb :'/users/create_user'
 	end
