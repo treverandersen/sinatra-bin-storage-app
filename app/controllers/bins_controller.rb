eed172f3a131a9045e5c1c124e200502dc18cb5f
treverandersen <trever.r.andersen@gmail.com>
2018-09-25 12:03:36 -0500

    setup bin controller action to post form data and gets the show page to show the created bin

--- app/controllers/bins_controller.rb
+++ app/controllers/bins_controller.rb
@@ -16,5 +16,27 @@ class BinsController < ApplicationController
 			redirect "/login"
 		end
 	end
-	
+
+	post '/bins' do 
+		if logged_in?
+			@bin = current_user.bins.build(name: params[:name], bushel_capacity: params[:bushel_capacity])
+			if @bin.save
+				redirect "/bins/#{@bin.id}"
+			else
+				redirect "/bins/new"
+			end
+		else
+			redirect "/login"
+		end
+	end
+
+	get '/bins/:id' do 
+		if logged_in?
+			@bin = Bin.find_by_id(params[:id])
+			erb :'/bins/show_bin'
+		else
+			redirect "/login"
+		end
+	end
+
 end
