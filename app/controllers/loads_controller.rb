45a67ca83a4fcf2f5648abb729c24a8676207520
treverandersen <trever.r.andersen@gmail.com>
2018-09-26 14:48:56 -0500

    lets a user edit and delete a load

--- app/controllers/loads_controller.rb
+++ app/controllers/loads_controller.rb
@@ -39,4 +39,46 @@ class LoadsController < ApplicationController
 		end
 	end
 
+	get '/loads/:id/edit' do
+		if logged_in?
+			@load = Load.find_by_id(params[:id])
+			if @load && @load.user == current_user
+				erb :'/loads/edit_load'
+			else
+				redirect "/loads"
+			end
+		else
+			redirect "/login"
+		end
+	end
+
+	patch '/loads/:id' do
+		if logged_in?
+			@load = Load.find_by_id(params[:id])
+			if @load && @load.user = current_user
+				if @load.update(driver_name: params[:driver_name], load_in_weight: params[:load_in_weight], load_out_weight: params[:load_out_weight])
+					redirect "/loads/#{@load.id}"
+				else
+					redirect "/loads/#{@load.id}/edit"
+				end
+			else
+				redirect "/loads"
+			end
+		else
+			redirect "/login"
+		end
+	end
+
+	get '/loads/:id/delete' do
+		if logged_in?
+			@load = Load.find_by_id(params[:id])
+			if @load && @load.user == current_user
+				@load.delete
+			end
+			redirect "/loads"
+		else
+			redirect "/login"
+		end
+	end
+
 end
