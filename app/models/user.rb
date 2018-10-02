f1aad4dd55f9c2f59254b4c548a85a8739f9bc73
treverandersen <trever.r.andersen@gmail.com>
2018-10-02 14:00:25 -0500

    cleaned up validation rule

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,5 @@ class User < ActiveRecord::Base
 	has_many :loads
 	has_secure_password
 	validates :username, presence: true
-	validates :username,
-    uniqueness: {
-      message: ->(object, data) do
-        "username is taken already!"
-      end
-    }
+	validates :username, uniqueness: true
 end
